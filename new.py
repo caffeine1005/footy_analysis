41947f909c55c682a8882794966374f8bed0f6b4
yuui0545 <yuui0545@uni.sydney.edu.au>
2025-12-28 16:36:36 +0900

    pass_network started

--- new.py
+++
@@ -1,7 +0,0 @@
-import polars as pl
-import soccerdata as sd
-import matplotlib.pyplot as plt
-import sklearn as skl
-
-fbref = sd.FBref()
-season_stats = fbref.read_team_season_stats(stat_type='shooting')
